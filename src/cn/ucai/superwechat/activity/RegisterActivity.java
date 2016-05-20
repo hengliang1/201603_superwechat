7937a83b9e842b6365aceba41086dc5051c1b1e6
hengliang1 <757178645@qq.com>
2016-05-20 15:21:46 +0800

    添加从相册选取图片并裁剪功能

--- src/cn/ucai/superwechat/activity/RegisterActivity.java
+++ src/cn/ucai/superwechat/activity/RegisterActivity.java
@@ -13,9 +13,11 @@
  */
 package cn.ucai.superwechat.activity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
@@ -25,8 +27,10 @@ import android.widget.Toast;
 import com.easemob.EMError;
 import com.easemob.chat.EMChatManager;
 
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.listener.OnSetAvatarListener;
 
 import com.easemob.exceptions.EaseMobException;
 
@@ -36,12 +40,15 @@ import com.easemob.exceptions.EaseMobException;
  */
 public class RegisterActivity extends BaseActivity {
 	private final static String TAG = RegisterActivity.class.getName();
-	Context mcontext;
+	Activity mcontext;
 	private EditText userNameEditText;
 	private EditText userNickEditText;
 	private EditText passwordEditText;
 	private EditText confirmPwdEditText;
 	ImageView mIVAvatar;
+	String avatarName;
+
+	OnSetAvatarListener mOnSetAvatarListener;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +63,30 @@ public class RegisterActivity extends BaseActivity {
 	private void setListener() {
 		OnSetRegisterListener();
 		OnSetLoginListener();
+		OnSetAvatarListener();
 	}
 
+	private void OnSetAvatarListener() {
+		findViewById(R.id.layout_user_avatar).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mOnSetAvatarListener = new OnSetAvatarListener(mcontext, R.id.layout_register, getAvatarName(), I.AVATAR_TYPE_USER_PATH);
+			}
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_OK) {
+			mOnSetAvatarListener.setAvatar(requestCode, data, mIVAvatar);
+		}
+	}
+
+	private String getAvatarName() {
+		avatarName = System.currentTimeMillis() + "";
+		return avatarName;
+	}
 	private void initView() {
 		userNameEditText = (EditText) findViewById(cn.ucai.superwechat.R.id.username);
 		passwordEditText = (EditText) findViewById(cn.ucai.superwechat.R.id.password);
