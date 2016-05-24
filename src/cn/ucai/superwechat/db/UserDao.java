b40862ea4d68f2bb07a53b4829aacee887e33f91
hengliang1 <757178645@qq.com>
2016-05-24 09:00:15 +0800

    修改UserDao包，需要卸载应用重新安装

--- src/cn/ucai/superwechat/db/UserDao.java
+++ src/cn/ucai/superwechat/db/UserDao.java
@@ -9,12 +9,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.bean.User;
 
-/**
- * 使用SQLiteOpenHelper方法创建数据库
- * SQLiteOpenHelper是一个抽象的数据库操作类，首先执行的是OnCreate
- * Created by sks on 2016/5/19.
- */
-public class UserDao extends SQLiteOpenHelper{
+
+public class UserDao extends SQLiteOpenHelper {
+    public static final String Id = "_id";
     public static final String TABLE_NAME = "user";
 
     public UserDao(Context context) {
@@ -23,75 +20,56 @@ public class UserDao extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = " DROP TABLE IF EXISTS " + I.User.TABLE_NAME + "" +
-                " CREATE TABLE " + I.User.TABLE_NAME +
-                I.User.USER_ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
-                I.User.USER_NAME + " TEXT NOT NULL," +
-                I.User.PASSWORD + " TEXT NOT NULL," +
-                I.User.NICK + "  TEXT NOT NULL," +
-                I.User.UN_READ_MSG_COUNT + " INTEGER DEFAULT 0"+");";
+        String sql = "create table if not exists "+ TABLE_NAME +"( " +
+                I.User.USER_ID +" integer primary key autoincrement, " +
+                I.User.USER_NAME +" varchar unique not null, " +
+                I.User.NICK +" varchar, " +
+                I.User.PASSWORD +" varchar, " +
+                I.User.UN_READ_MSG_COUNT +" int default(0) " +
+                ");";
+        db.execSQL(sql);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
     }
 
-    /**
-     * SQLiteOpenHelper封装了一个insert方法可以方便我们执行插入行为
-     * @param user
-     * @return
-     */
-    public boolean addUser(User user) {
+    public boolean addUser(User user){
         ContentValues values = new ContentValues();
-        values.put(I.User.USER_ID, user.getMUserId());
-        values.put(I.User.USER_NAME, user.getMUserName());
-        values.put(I.User.PASSWORD, user.getMUserPassword());
-        values.put(I.User.NICK, user.getMUserNick());
-        values.put(I.User.UN_READ_MSG_COUNT, user.getMUserUnreadMsgCount());
-        //调用getWritableDatabase方法真正创建一个数据库
+        values.put(I.User.USER_ID,user.getMUserId());
+        values.put(I.User.USER_NAME,user.getMUserName());
+        values.put(I.User.NICK,user.getMUserNick());
+        values.put(I.User.PASSWORD,user.getMUserPassword());
+        values.put(I.User.UN_READ_MSG_COUNT,user.getMUserUnreadMsgCount());
         SQLiteDatabase db = getWritableDatabase();
-        long insert = db.insert(I.User.TABLE_NAME, null, values);
-        return insert > 0;
+        long insert = db.insert(TABLE_NAME, null, values);
+        return insert>0;
     }
 
-    /**
-     * 根据用户名查找用户
-     * @param username
-     * @return
-     */
-    public User findUserByName(String username) {
-        //创建数据库
+    public User findUserByUserName(String userName){
         SQLiteDatabase db = getReadableDatabase();
-        //获取sql语句
-        String sql = "select * from " + TABLE_NAME + " where " + I.User.USER_NAME + "=?";
-        //利用光标Cursor，Cursor指向当前的数据记录，然后可以从光标中获取相应的数据,执行本地sql语句查询
-        Cursor c = db.rawQuery(sql, new String[]{username});
-        if (c.moveToNext()) {
+        String sql = "select * from "+ TABLE_NAME + " where " + I.User.USER_NAME  + "=?";
+        Cursor c = db.rawQuery(sql,new String []{userName});
+        if(c.moveToNext()){
             int uid = c.getInt(c.getColumnIndex(I.User.USER_ID));
             String nick = c.getString(c.getColumnIndex(I.User.NICK));
             String password = c.getString(c.getColumnIndex(I.User.PASSWORD));
             int unReaderMsgCount = c.getInt(c.getColumnIndex(I.User.UN_READ_MSG_COUNT));
-            return new User(uid, username, password, nick, unReaderMsgCount);
+            return new User(uid,userName,password,nick,unReaderMsgCount);
         }
         c.close();
         return null;
     }
-    /**
-     * 更新用户
-     * @param user
-     * @return
-     */
-    public boolean updateUser(User user) {
+
+    public boolean updateUser(User user){
         ContentValues values = new ContentValues();
-        values.put(I.User.USER_ID, user.getMUserId());
-        values.put(I.User.NICK, user.getMUserNick());
-        values.put(I.User.PASSWORD, user.getMUserPassword());
-        values.put(I.User.UN_READ_MSG_COUNT, user.getMUserUnreadMsgCount());
+        values.put(I.User.USER_ID,user.getMUserId());
+        values.put(I.User.USER_NAME,user.getMUserName());
+        values.put(I.User.NICK,user.getMUserNick());
+        values.put(I.User.PASSWORD,user.getMUserPassword());
+        values.put(I.User.UN_READ_MSG_COUNT,user.getMUserUnreadMsgCount());
         SQLiteDatabase db = getWritableDatabase();
-        long insert = db.update(I.User.TABLE_NAME, values, "where " + I.User.USER_NAME + "=?", new String[]{user.getMUserName()});
-        return insert > 0;
+        long insert = db.update(TABLE_NAME, values,I.User.USER_NAME+"=?",new String[]{user.getMUserName()});
+        return insert>0;
     }
-
-
 }
